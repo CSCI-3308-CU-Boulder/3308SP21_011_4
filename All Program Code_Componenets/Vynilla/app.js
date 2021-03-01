eae0806b13994a6a1c1f098ac5f3aa5da3a40fea
jakelevato2022 <jale4181@colorado.edu>
2021-03-01 09:41:06 -0700

    Registration

---
+++ All Program Code_Componenets/Vynilla/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+dotenv.config({ path: './.env'});
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE
+});
+
+db.connect((error) => {
+    if(error) {
+        console.log(error)
+    } else {
+        console.log("MySQL Connected...")
+    }
+})
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+
+// parse application/x-www-form-urlencoded, basically can only parse incoming Request Object if strings or arrays
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.set('view engine', 'hbs');
+
+//Define Routes
+app.use('/', require('./routes/pages.js'));
+app.use('/auth', require('./routes/auth'));
+
+app.listen(5000, () => {
+    console.log("Server Started on Port 5000")
+})
---
+++ All Program Code_Componenets/Vynilla/routes/pages.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) =>{
+    res.render('index');
+});
+
+router.get('/register', (req, res) =>{
+    res.render('register');
+});
+
+router.get('/feed', (req, res) =>{
+    res.render('feed');
+});
+
+
+module.exports = router;
