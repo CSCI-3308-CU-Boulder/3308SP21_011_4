a0cb2ecdbaad70d620a8357f67de44f20ca016ca
Jared Jewell <jaje9434@colorado.edu>
2021-03-28 13:17:22 -0600

    added song.js, playlist-and-song.js
    p-and-song.js allows creation of a playlist, searching & addition of
    songs to that playlist

--- All Program Code_Componenets/Vynilla/Spotify-API-examples/playlist-and-song.js
+++ All Program Code_Componenets/Vynilla/Spotify-API-examples/playlist-and-song.js
@@ -1,8 +1,9 @@
 const SpotWAPI = require("spotify-web-api-node");
 const express = require("express");
-const playlistName = "Hacking....";
-const playlistDesc = "Get HACKED.";
-const artist = "Doja Cat";
+// const playlistName = "Hacking....";
+// const playlistDesc = "Get HACKED.";
+const songs = ["Say So", "With Arms Wide Open", "Bleecker Street", "Heartless", "(Don't Fear) The Reaper",
+               "Human After All", "Interstate Love Song", "Great Dane", "Cherub Rock", "Beverly Hills"];
 
 const app = express();
 
@@ -10,20 +11,17 @@ const credentials = {
     clientId: "1721ccaf9f0f40a196710dede9030908",
     clientSecret: "7efbade01f16446a880254fe1f30d2a7",
     redirectUri: "http://localhost:8888/callback",
-}
+};
 
-const scopes = [
-    "playlist-modify-public"
-]
+const scopes = ["playlist-modify-public"];
 
 var spotifyApi = new SpotWAPI(credentials);
 
-app.get("/login", (req,res) => {
+app.get("/login", (req, res) => {
     res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
 
-
-app.get("/callback", (req,res) => {
+app.get("/callback", (req, res) => {
     var code = req.query.code;
     var refresh = req.query.refresh;
     var error = req.query.error;
@@ -34,7 +32,8 @@ app.get("/callback", (req,res) => {
         return;
     }
 
-    spotifyApi.authorizationCodeGrant(code)
+    spotifyApi
+        .authorizationCodeGrant(code)
         .then((data) => {
             const access_token = data.body["access_token"];
             const refresh_token = data.body["refresh_token"];
@@ -43,56 +42,43 @@ app.get("/callback", (req,res) => {
             spotifyApi.setAccessToken(access_token);
             spotifyApi.setRefreshToken(refresh_token);
 
-            //search for 5 of doja's songs
-            spotifyApi.searchTracks('artist:'+artist,
-                { limit : 5 }
-            )
-
-            //structure of 'data':
-            //{
-            // body: {
-            //   tracks: {
-            //     href: 'https://api.spotify.com/v1/search?query=artist%3ADoja+Cat&type=track&offset=0&limit=20',
-            //     items: [Array],
-            //     limit: 20,
-            //     next: 'https://api.spotify.com/v1/search?query=artist%3ADoja+Cat&type=track&offset=20&limit=20',
-            //     offset: 0,
-            //     previous: null,
-            //     total: 140
-            //   }
-            // },
-            // headers: {
-            //   'content-type': 'application/json; charset=utf-8',
-            //   'cache-control': 'public, max-age=7200',
-            //   ...
-            //   'transfer-encoding': 'chunked'
-            // },
-            // statusCode: 200
-            // }
-
-                .then((data) => {
-                    // console.log('Searching tracks by ' + artist);
-                    // console.log('\nTracks by: ' + artist);
-                    // console.log(data.body.tracks.items);
-                    data.body.tracks.items.forEach(song => {
-                        // console.log(song.name + ", id: " + song.id);
-                        songsArr.push("spotify:track:"+song.id);
+            songs.forEach(song => {
+                spotifyApi.searchTracks(song, {limit:1})
+                    .then((data) => {
+                        console.log(data.body.tracks.items[0].name, " ", data.body.tracks.items[0].id);
+                        console.log(data.body.tracks.items[0].artists[0].name);
+                        songsArr.push('spotify:track:'+data.body.tracks.items[0].id);
                     })
-                    return songsArr;
-                }).then((songs) => {
-                    console.log(songs);
-                })
+
+            })
+
+            return songsArr;
         })
-        .then( () => {
-            console.log("\n:D");
-        }
-         )
-        .catch((err) => {
-            console.error(`Problem getting tokens: ${error}`);
-            res.send(`Problem getting tokens: ${error}`);
-        })
-})
+               //take songsArr as 'songs'
+               .then((songs) => {
 
-app.listen(8888, console.log("Sweet, let's find some songs by " + artist +".\n Go visit http://localhost:8888/login"));
+                   //create a playlist
+                   return spotifyApi
+                       .createPlaylist("Hacking... :O", {
+                           description: "testing!",
+                           public: true,
+                       })
 
+                       //data = json obj. of playlist info.
+                       //we need the playlistid to add songs to it
+                       .then((data) => {
+                           // console.log(data.body.id);
+                           // add songs to playlist we just created
+                           return spotifyApi.addTracksToPlaylist(data.body.id, songs);
+                       })
+                       .catch((err) => {
+                           console.log("oops!: ", err);
+                       });
+               })
+               .catch((err) => {
+                   console.log("Oh crap!:", err);
+               });
 
+});
+
+app.listen(8888, console.log("Sweet, let's find those songs!! \n Go visit http://localhost:8888/login"));
