61139760a2f914f9ce1d17dd952b5ed614096122
Jared Jewell <jaje9434@colorado.edu>
2021-04-05 11:34:39 -0600

    changed it to JSON <3<3<3

--- All Program Code_Componenets/Vynilla/Spotify-API-examples/modal-attempt/modal.js
+++ All Program Code_Componenets/Vynilla/Spotify-API-examples/modal-attempt/modal.js
@@ -15,19 +15,12 @@ const scopes = [];
 
 var spotifyApi = new SpotWAPI(credentials);
 
-app.get("/modal");
+//auth ourselves
 app.get("/login", (req, res) => {
     res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
 
 app.get("/callback", (req, res) => {
-    var trackname = "Say So";
-    var artistname = "Doja Cat";
-    // var trackname = req.query.trackname;
-    // var artistname = req.query.artistname;
-    // trackname = trackname.replace("+", " ");
-    // artistname = artistname.replace("+", " ");
-
     var code = req.query.code;
     var error = req.query.error;
 
@@ -40,48 +33,62 @@ app.get("/callback", (req, res) => {
         .authorizationCodeGrant(code)
         .then((data) => {
             const access_token = data.body["access_token"];
-            spotifyApi.setAccessToken(access_token);
-
-            // spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar')
-            spotifyApi
-                .searchTracks("track:" + trackname + " artist:" + artistname)
-                .then((data) => {
-                    res.render("modal", { songs: [] });
-                })
-                .catch((err) => {
-                    console.log("Couldn't get tracks?? uhh: " + err);
-                    return;
-                });
+            spotifyApi.setAccessToken(access_token); //globally auth ourselves to access 'search'
+            //then....
+            res.render("modal", { songs: [] }); //present the user w/ the modal.hbs view
         })
         .catch((err) => {
             console.log("Couldn't auth!! : " + err);
         });
 });
 
+// entering a songname & pressing search on the form sends a get req. to
+// /callback/search w/ songname in url
 app.get("/callback/search", (req, res) => {
     var trackname = req.query.songname;
-    var artistname = req.query.artistname;
-    trackname = trackname.replace("+", " ");
-    artistname = artistname.replace("+", " ");
-    console.log(trackname, artistname);
-    songs = []
+    trackname = trackname.replace("+", " "); //want the search term to be in plain english
+    // songs = [];
+    var songsObj = {
+        "songs": []
+    };
 
-    spotifyApi.searchTracks("track:" + trackname + " artist:" + artistname)
+    spotifyApi
+        .searchTracks(trackname) //search the terms from the URL
         .then((data) => {
-            console.log(data);
-            console.log(data.body);
+            //many songs are returned. iterate thru them
             data.body.tracks.items.forEach((song) => {
-                // console.log(": " + song.name);
-                songs.push(song.name);
-            })
+                //for each song...
+                var artists = []; //who wrote it
 
-            res.render('modal', {songs:songs});
+                //for each artist on this song...
+                song.artists.forEach((artist) => {
+                    artists.push(artist.name); //add them to artists
+                });
+
+
+                //add an entry for the current song
+                //whose 'artists' field is an array of all artists
+                console.log(songsObj);
+                songsObj.songs.push({
+                    songname: song.name,
+                    artists: [...artists] //spread operator. kinda funky, definitely redundant, but it feels flex
+                });
+            });
+
+            res.render("modal", { songs: songsObj["songs"] }); //once that's all done, render it in modal.hbs
         })
         .catch((err) => {
             console.log("So close!! : " + err);
             return;
-        })
-
+        });
 });
 
 app.listen(8888, console.log("Goto http://localhost:8888/login"));
+
+//meeting notes? lol
+// jared - song search & live song display or what ur listening to
+// bernardo - "      "
+// jake  - profile picture stuff
+// dalbir - profile page friends selector
+// bo - hasn't responded, hasn't been active
+// todo - work on integrating it all
