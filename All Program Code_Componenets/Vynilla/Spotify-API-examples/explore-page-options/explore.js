b618ead64be8c2e17e47c93dfd55d317296ed141
Jared Jewell <jaje9434@colorado.edu>
2021-04-14 14:59:35 -0600

    (using pg) working queue page

--- All Program Code_Componenets/Vynilla/Spotify-API-examples/explore-page-options/explore.js
+++ All Program Code_Componenets/Vynilla/Spotify-API-examples/explore-page-options/explore.js
@@ -2,13 +2,28 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const SpotWAPI = require("spotify-web-api-node");
-const queryString = require("querystring");
+const bodyParser = require('body-parser');
+const pgp = require("pg-promise")();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
+
+const dbConfig = {
+    host: 'localhost',
+    port: 5432,
+    database: 'example_proj_db',
+    user: 'postgres',
+    password: '4266'
+};
+
+var db = pgp(dbConfig);
 
  const credentials = {
      clientId: "1721ccaf9f0f40a196710dede9030908",
@@ -45,41 +60,125 @@ app.get("/callback", (req, res) => {
 });
 
 app.get("/queue", (req,res) => {
-    res.render("queue", {friends:["Bernardo", "Dalbir", "Jake"]});
+    var friendsQuery = 'SELECT person FROM friends;';
+    db.any(friendsQuery)
+        .then((data) => {
+            console.table(data);
+            var friendsArr = [];
+            data.forEach((friend) => {
+                friendsArr.push(friend.person);
+            })
+            res.render("queue", {friends: friendsArr});
+        })
+        .catch((err) => {
+            console.error(err);
+            res.render("queue", {friends: []})
+        })
 })
 
 app.get('/select-friend', (req,res) => {
     const friendname = req.query["friends-dropdown"];
-    var friendObj = {
-        name: friendname,
-        playlistid: 1,
-        queueSoFar: []
-    }
+    const friendQuery = "SELECT * FROM friends WHERE person='"+friendname+"';";
+    const friendsQuery = "SELECT * FROM friends;";
+    var friends = [];
 
-    const theirq = req.query["theirqueue"];
+    db.any(friendsQuery).then((data) => {
+        data.forEach((friend) => {
+            friends.push(friend.person);
+        })
+    })
+    .catch((err) => {console.error(err)});
 
-    var songsObj = {
-        songs: [
-            {
-                songname: "Poggoli", artist: ["Rav", "ioli"]
-            },
-            {
-                songname: "Say So", artist: ["Doja Cat"]
+    db.any(friendQuery)
+        .then((friend)=>{
+            var friend = friend[0];
+            var name = friend.person;
+            var their_q = friend.playlistid;
+            if (their_q == 'none') {
+                res.render('queue', {
+                    friend: name,
+                    theirQueue: false,
+                    friends: friends
+                })
+                return;
+            } else {
+                var songsObj = {
+                    songs : []
+                }
+                //get their queue
+                console.log("In "+name+"'s queue...");
+                spotifyApi.getPlaylist(their_q).then((data) => {
+                    const playlistTracks = data.body.tracks.items;
+                    playlistTracks.forEach((song) => {
+                        var artists = [];
+                        console.log(song.track.name);
+                        song.track.artists.forEach((artist) => {
+                            artists.push(artist.name);
+                            console.log(" --- " + artist.name);
+                        })
+                        songsObj["songs"].push({
+                            songname: song.track.name,
+                            artists: artists
+                        })
+                    })
+                    res.render('queue', {
+                        friend: name,
+                        theirQueue: true,
+                        friends: friends,
+                        currentQueue: songsObj["songs"]
+                    })
+
+                })
+                .catch((err) => {
+                    console.error(err);
+                })
+                console.log("-----outhere ------")
+                console.log(songsObj["songs"])
+                // res.render('queue', {
+                //     friend: name,
+                //     theirQueue: true,
+                //     friends: friends,
+                //     currentQueue: songsObj["songs"]
+                // })
             }
-        ]
-    }
+        })
+    .catch((err)=>{console.error(err)});
 
-    res.render("queue", {friend: friendObj, queue: songsObj["songs"], friends: ["Bernardo", "Dalbir", "Jake"]});
 })
+
+app.get("/make_queue", (req, res) => {
+    var friendname = req.query["friendname"];
+    var queuename = req.query["queue_name"];
+
+    const getTheirFriends = "SELECT * FROM friends;"; //obvi we wanna use a relationship table or w/e
+    const friends = [];
+
+    db.any(getTheirFriends).then((data) => {
+        data.forEach((friend) => {
+            friends.push(friend.person);
+        })
+    })
+    spotifyApi.createPlaylist(queuename, {'description': "Made with Vynilla!", 'public' : true})
+        .then((data) => {
+            // console.log(data);
+            var queueid = data.body.id;
+            var query = "update friends set playlistid = '"+queueid+"' where person = '"+friendname+"';"
+            db.any(query)
+                .then((data) => {
+                    res.render('queue', {
+                        friend: friendname,
+                        theirQueue: true,
+                        friends: friends
+                    })
+                })
+                .catch((err) => {console.error(err)})
+        })
+})
+
 
 app.get("/search", (req,res) => {
     const songname = req.query["songname"];
     const friendname = req.query["friendname"];
-    var friendObj = {
-        name: friendname,
-        playlistid: 1,
-        queueSoFar: []
-    }
 
     var songsObj = {
         songs: []
@@ -87,7 +186,7 @@ app.get("/search", (req,res) => {
     spotifyApi.searchTracks(songname, {limit: 5}).then((data) => {
         data.body.tracks.items.forEach((track) => {
             var artists = [];
-            console.log(track.name);
+            // console.log(track.name);
             track.artists.forEach((artist) => {
                 // console.log(artist.name);
                 artists.push(artist.name);
@@ -99,17 +198,69 @@ app.get("/search", (req,res) => {
             })
 
         })
-        console.log(songsObj["songs"])
+        console.table(songsObj["songs"])
         res.render("queue", {
             songsFromSearch:songsObj["songs"], //obvi we need to get this from DB
             queueSoFar: songsObj["songs"],//& this
-            friend: friendObj,
+            friend: friendname,
             friends: ["Bernardo", "Dalbir", "Jake"]
         });
     }).catch((err) => {
         console.log("Shoot!" + err);
         return;
     })
+})
+
+app.get('/addToPlaylist', (req,res) => {
+    const song = req.query["songLink"];
+    const name = req.query["friendname"];
+
+    const getTheirFriends = "SELECT * FROM friends;"; //obvi we wanna use a relationship table or w/e
+    const friends = [];
+
+    db.any(getTheirFriends).then((data) => {
+        data.forEach((friend) => {
+            friends.push(friend.person);
+        })
+    })
+    .catch((err) => {console.error(err)})
+
+    const getFriendPlaylist = "SELECT playlistid FROM friends WHERE person='"+name+"';";
+
+    db.any(getFriendPlaylist)
+        .then((data) => {
+            const theirq = data[0].playlistid;
+            spotifyApi.addTracksToPlaylist(theirq, [song]).then((data) => {
+                console.log("Added song...");
+            })
+                .then(() => {
+                    var songsObj = {
+                        songs: []
+                    }
+                    spotifyApi.getPlaylist(theirq)
+                        .then((data) => {
+                            const playlistTracks = data.body.tracks.items;
+                            playlistTracks.forEach((song) => {
+                                var artists = [];
+                                song.track.artists.forEach((artist) => {
+                                    artists.push(artist.name);
+                                })
+                                songsObj["songs"].push({
+                                    songname: song.track.name,
+                                    artists: artists
+                                })
+                            })
+                        res.render('queue', {
+                            friend: name,
+                            theirQueue: true,
+                            friends: friends,
+                            currentQueue: songsObj["songs"]
+                        })
+                    })
+                })
+        .catch((err) => {console.error(err)})
+
+        })
 })
 
 app.listen(
