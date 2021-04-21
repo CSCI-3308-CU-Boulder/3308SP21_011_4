9ff40fdca047bc44c65657c96278a0fb9c0e833b
jakelevato2022 <jale4181@colorado.edu>
2021-04-20 19:06:02 -0600

    db error

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ const app = express();
 //     password: process.env.DATABASE_PASS,
 //     database: process.env.DATABASE
 // });
+const db = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE
+  });
 
 //DISCONNET BUG ??
 var db_config = {
@@ -27,31 +33,31 @@ var db_config = {
     database: process.env.DATABASE
   };
 
-var db;
+// var db;
 
-function handleDisconnect() {
-db = mysql.createConnection(db_config); // Recreate the connection, since
-                                                // the old one cannot be reused.
+// function handleDisconnect() {
+// db = mysql.createConnection(db_config); // Recreate the connection, since
+//                                                 // the old one cannot be reused.
 
-db.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-    console.log('error when connecting to db:', err);
-    setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-});                                     // process asynchronous requests in the meantime.
-                                        // If you're also serving http, display a 503 error.
-db.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-    handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-    throw err;                                  // server variable configures this)
-    }
-});
-}
+// db.connect(function(err) {              // The server is either down
+//     if(err) {                                     // or restarting (takes a while sometimes).
+//     console.log('error when connecting to db:', err);
+//     setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+//     }                                     // to avoid a hot loop, and to allow our node script to
+// });                                     // process asynchronous requests in the meantime.
+//                                         // If you're also serving http, display a 503 error.
+// db.on('error', function(err) {
+//     console.log('db error', err);
+//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+//     handleDisconnect();                         // lost due to either server restart, or a
+//     } else {                                      // connnection idle timeout (the wait_timeout
+//     throw err;                                  // server variable configures this)
+//     }
+// });
+// }
 
-handleDisconnect();
-  //DISCONNECT BUG FIX
+// handleDisconnect();
+//   //DISCONNECT BUG FIX
 
 //create a spotifyAPI instance w/ our credentials
 const spotifyApi = new SpotifyWebApi({
