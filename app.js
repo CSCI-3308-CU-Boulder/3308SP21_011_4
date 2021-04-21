f2b2480cf4ccaa4b35c58677f46266e26d52b8b0
jakelevato2022 <jale4181@colorado.edu>
2021-04-20 18:55:30 -0600

    db error

--- app.js
+++ app.js
@@ -27,20 +27,20 @@ var db_config = {
     database: process.env.DATABASE
   };
 
-var connection;
+var db;
 
 function handleDisconnect() {
-connection = mysql.createConnection(db_config); // Recreate the connection, since
+db = mysql.createConnection(db_config); // Recreate the connection, since
                                                 // the old one cannot be reused.
 
-connection.connect(function(err) {              // The server is either down
+db.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
     console.log('error when connecting to db:', err);
     setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
 });                                     // process asynchronous requests in the meantime.
                                         // If you're also serving http, display a 503 error.
-connection.on('error', function(err) {
+db.on('error', function(err) {
     console.log('db error', err);
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
     handleDisconnect();                         // lost due to either server restart, or a
